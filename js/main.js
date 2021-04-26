c89d00d3a68d059e2607473dd34c10ee2f1a5bce
GSTrendafilov19 <GSTrendafilov19@codingburgas.bg>
2021-04-26 22:39:09 +0300

    Add function that will execute on submit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let arr=[10, 15, 20, 30]
+let random = arr[Math.floor(Math.random() * arr.length)]
+function est() {
+    alert(`Благодарим за докладването! Очаквайте след около ${random} минути да пристигне екип на пожарна`)
+}
